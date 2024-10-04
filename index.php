2fa409c4087d674603c55cbc654d7e1092ebec9c
Arthur A. Ferreira <arthur.ferreira@ufabc.edu.br>
2024-10-04 13:20:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-// Configurações do banco de dados MySQL
-$host = 'localhost';
-$dbname = 'test_db';
-$username = 'root';
-$password = 'admin';
-
-// Conectar ao banco de dados MySQL
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h2>Conexão bem-sucedida ao banco de dados MySQL!</h2>";
-} catch (PDOException $e) {
-    echo "<h2>Falha ao conectar ao banco de dados:</h2> " . $e->getMessage();
-}
 
 // Exibir uma mensagem simples
 echo "<h1>Bem-vindo à minha aplicação PHP!</h1>";
